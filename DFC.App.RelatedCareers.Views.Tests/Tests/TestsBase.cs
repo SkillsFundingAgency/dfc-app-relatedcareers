b865ccb0b842d9fd306f67869a4b09ec103ce2c8
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-09-09 15:39:33 +0100

    Added View unit tests (#3)

---
+++ DFC.App.RelatedCareers.Views.Tests/Tests/TestsBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DFC.App.RelatedCareers.Views.Tests.Tests
+{
+    public class TestsBase
+    {
+        protected string viewRootPath;
+
+        public TestsBase()
+        {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("appsettings.json");
+            var configuration = config.Build();
+
+            viewRootPath = configuration["ViewRootPath"];
+        }
+    }
+}
