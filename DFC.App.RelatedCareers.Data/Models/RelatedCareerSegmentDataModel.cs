eaa25137f890f221f76935f2dcbc60ebf322a103
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-11-12 15:07:24 +0000

    Corrected location of LastReviewed date (#39)

--- DFC.App.RelatedCareers.Data/Models/RelatedCareerSegmentDataModel.cs
+++ DFC.App.RelatedCareers.Data/Models/RelatedCareerSegmentDataModel.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DFC.App.RelatedCareers.Data.Models
 {
     public class RelatedCareerSegmentDataModel
     {
         public const string SegmentName = "RelatedCareers";
+
+        public DateTime LastReviewed { get; set; }
 
         public IEnumerable<RelatedCareerDataModel> RelatedCareers { get; set; }
     }
