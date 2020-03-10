5ceaec7559d352d65f734afe5143f6a4d1bd4c10
James Barnes <james@testtrends.co.uk>
2020-03-10 11:52:23 +0000

    Story/DFC-11486 API integration tests (#62)
    * Added framework
    
    * Test written and passing
    
    * Dev complete
    
    * Finished. Test working.
    
    * Uncommitted files.
    
    * Reduced .NET version to 2.2
    
    * Removed redundant projects.

---
+++ DFC.App.RelatedCareers.Tests.IntegrationTests.API/Model/API/RelatedCareersResponseBody.cs
@@ -0,0 +1,8 @@
+﻿namespace DFC.App.RelatedCareers.Tests.IntegrationTests.API.Model.API
+{
+    public class RelatedCareersResponseBody
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+}
