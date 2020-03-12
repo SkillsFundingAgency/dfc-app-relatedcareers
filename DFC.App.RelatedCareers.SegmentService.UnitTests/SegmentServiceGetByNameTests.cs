fe904758464fc51dd5aaf36f82e69cbc33e2017d
vallabha <vallabha24@gmail.com>
2020-03-12 10:43:38 +0000

    Commented out view tests

--- DFC.App.RelatedCareers.SegmentService.UnitTests/SegmentServiceGetByNameTests.cs
+++ DFC.App.RelatedCareers.SegmentService.UnitTests/SegmentServiceGetByNameTests.cs
@@ -47,7 +47,7 @@ namespace DFC.App.RelatedCareers.SegmentService.UnitTests
             var exceptionResult = await Assert.ThrowsAsync<ArgumentNullException>(async () => await relatedCareersSegmentService.GetByNameAsync(null).ConfigureAwait(false)).ConfigureAwait(false);
 
             // assert
-            Assert.Equal("Value cannot be null.\r\nParameter name: canonicalName", exceptionResult.Message);
+            Assert.Equal("Value cannot be null. (Parameter 'canonicalName')", exceptionResult.Message);
         }
 
         [Fact]
