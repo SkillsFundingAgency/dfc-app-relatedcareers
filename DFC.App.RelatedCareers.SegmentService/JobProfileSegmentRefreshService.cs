01d2a129a0c8b3b267038e584832b58ffecaaaae
hnandra <32364605+hnandra@users.noreply.github.com>
2023-06-01 14:33:43 +0100

    added logging to jobprofilesegmentrefreshservice

--- DFC.App.RelatedCareers.SegmentService/JobProfileSegmentRefreshService.cs
+++ DFC.App.RelatedCareers.SegmentService/JobProfileSegmentRefreshService.cs
@@ -13,6 +13,7 @@ namespace DFC.App.RelatedCareers.SegmentService
         private const int BatchSize = 500;
         private readonly ITopicClient topicClient;
         private readonly ICorrelationIdProvider correlationIdProvider;
+        private readonly ILogService logService;
 
         public JobProfileSegmentRefreshService(ITopicClient topicClient, ICorrelationIdProvider correlationIdProvider)
         {
@@ -22,12 +23,17 @@ namespace DFC.App.RelatedCareers.SegmentService
 
         public async Task SendMessageAsync(TModel model)
         {
+            logService.LogInformation($"{nameof(SendMessageAsync)} has been called");
+
             var message = CreateMessage(model);
             await topicClient.SendAsync(message).ConfigureAwait(false);
+            logService.LogInformation($"{nameof(message)} has been created");
         }
 
         public async Task SendMessageListAsync(IList<TModel> models)
         {
+            logService.LogInformation($"{nameof(SendMessageListAsync)} has been called");
+
             // List is batched to avoid exceeding the Service Bus size limit on DEV and SIT of 256KB
             if (models != null)
             {
@@ -38,11 +44,15 @@ namespace DFC.App.RelatedCareers.SegmentService
                     var batchedList = listOfMessages.Skip(i).Take(BatchSize).ToList();
                     await topicClient.SendAsync(batchedList).ConfigureAwait(false);
                 }
+
+                logService.LogInformation($"{nameof(listOfMessages)} has been created");
             }
         }
 
         private Message CreateMessage(TModel model)
         {
+            logService.LogInformation($"{nameof(CreateMessage)} has been called");
+
             var messageJson = JsonConvert.SerializeObject(model);
             return new Message(Encoding.UTF8.GetBytes(messageJson))
             {
